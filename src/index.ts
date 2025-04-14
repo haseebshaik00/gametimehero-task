5ee257f2f67de135fbe3bc9487068177fedbe661
haseebshaik00 <haseebshaik00@gmail.com>
2025-04-14 15:50:31 -0700

    Refactor index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { ConsoleLogger } from './utils/Logger';
 const logger = new ConsoleLogger();
 const service = new RsvpService(logger);
 
+console.log("\nAttendees Response Logs:")
 service.addOrUpdate({ id: '1', name: 'Haseeb' }, 'Yes');
 service.addOrUpdate({ id: '2', name: 'Alex' }, 'No');
 service.addOrUpdate({ id: '3', name: 'Hailey' }, 'Maybe');
@@ -12,6 +13,8 @@ service.addOrUpdate({ id: '3', name: 'Phil' }, 'No');
 service.addOrUpdate({ id: '3', name: 'Claire' }, 'Maybe');
 service.addOrUpdate({ id: '3', name: 'Lily' }, 'Yes');
 
-console.log("")
+console.log("\nConfimed Attendees of the Event:")
 console.log(service.getConfirmedAttendees());
+
+console.log("\nResponses Count:")
 console.log(service.countResponses());
